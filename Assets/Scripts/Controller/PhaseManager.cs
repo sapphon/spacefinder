fa8552d7b0363e9a55ab0ea8fa19b3bfcfe594be
Sapphon <sapphon@gmail.com>
2021-12-06 02:30:59 -0500

    Phases now rotate through the 3 options [https://trello.com/c/2W758vVn]

---
+++ Assets/Scripts/Controller/PhaseManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseManager : MonoBehaviour
+{
+    protected Phase currentPhase = Phase.Helm;
+    protected List<Ship> shipsSignalingComplete = new List<Ship>();
+
+    public Phase GetCurrentPhase()
+    {
+        return this.currentPhase;
+    }
+
+    public bool TryAdvancePhase()
+    {
+        bool mayAdvance = CanPhaseEnd();
+        if (mayAdvance)
+        {
+            EndPhase();
+        }
+
+        return mayAdvance;
+
+    }
+
+    protected bool CanPhaseEnd()
+    {
+        Ship[] allShips = Ship.getAllShips();
+        return allShips.Length == shipsSignalingComplete.Count && new HashSet<Ship>(allShips).SetEquals(allShips);
+    }
+
+    protected void EndPhase()
+    {
+        this.currentPhase = (Phase)(((int)this.currentPhase + 1) % Enum.GetNames(typeof(Phase)).Length);
+    }
+}
+
+public enum Phase
+{
+    Engineering=0, Helm=1, Gunnery=2
+}
---
+++ Assets/Scripts/View/PhasePanelUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhasePanelUI : MonoBehaviour
+{
+    private Text _phaseNameText;
+    private PhaseManager _phaseManager;
+
+    void Awake()
+    {
+        this._phaseManager = FindObjectOfType<PhaseManager>();
+        this._phaseNameText = transform.Find("CurrentPhaseReadout").GetComponent<Text>();
+        this.transform.Find("AdvancePhaseButton").GetComponent<Button>().onClick.AddListener(TryAdvancePhase);
+    }
+
+    void Update()
+    {
+        _phaseNameText.text = _phaseManager.GetCurrentPhase().ToString();
+    }
+
+    public void TryAdvancePhase()
+    {
+        bool phaseAdvanced = _phaseManager.TryAdvancePhase();
+        if (!phaseAdvanced)
+        {
+            Debug.Log("Phase not advanced; not all ships ready");
+        }
+    }
+}
